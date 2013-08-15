871bf6763c1d64923ce2ad9f5a5bb0cda5ae5401
James Holliday <jrholliday@ws1.(none)>
2013-08-15 12:32:04 -0700

    Added license info.

--- GoldenAge/__init__.py
+++ GoldenAge/__init__.py
@@ -2,6 +2,23 @@ import cPickle as _pickle
 
 from Book import Book
 
+def license():
+    """Display license and copyright notice for the GoldenAge package."""
+
+    import pydoc
+
+    # Read in the license text
+    file = open( path + '/license.txt' )
+    text = file.read()
+    file.close()
+
+    # Use python pager to display the license text
+    pydoc.pager(text)
+
+    # End license()
+    return None
+
+
 def load(filename):
     book = _pickle.load(open(filename))
     return book
