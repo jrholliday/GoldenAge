b4c0d1bfaa9db8305d4d5fd0ffff2a00a78dd320
James R Holliday <jrholliday@gmail.com>
2013-08-21 12:22:24 -0700

    Documentation

--- GoldenAge/Book.py
+++ GoldenAge/Book.py
@@ -7,16 +7,19 @@ import random
 
 import os
 
+###############################################################################
+
 class Book(object):
+    """Collection of linked nodes/writtings.  Taken together, they form a Book"""
     def __init__(self, title):
+        """Initialize a new Book object with a specified title"""
         self._BEGIN = datetime.datetime.now().isoformat()
         self._END   = datetime.datetime.max.isoformat()
         self._END   = datetime.datetime.min.isoformat()
         # Initialize Book members
         self._title  = str(title).strip()
-        self._desc   = str()
         self._author = list()
-        self._meta   = dict()
+        self._meta   = dict({'desc':str()})
         self._nodes  = dict()
         # Create "Book Cover"
         self._nodes[self._BEGIN] = Node()
@@ -29,51 +32,74 @@ class Book(object):
         #
         return None
 
+    #-------------------------------------------------------------------------#
+
     def get_info(self):
-        first = dateutil.parser.parse(
+        """Return a formatted description of The Book (perfect for dust jackets)"""
+        first_edit = dateutil.parser.parse(
             sorted(self._nodes.keys())[ 0]).strftime("%d %b %Y")
-        last  = dateutil.parser.parse(
+        last_edit  = dateutil.parser.parse(
             sorted(self._nodes.keys())[-1]).strftime("%d %b %Y")
-        info  = ""
+        info  = str()
         #
         info += self._title + "\n\n"
-        info += self._desc + "\n\n"
+        #
+        if self._meta['desc'] != "":
+            info += self._meta["desc"] + "\n\n"
         for author in self._author:
             info += author + "\n"
         info += "\n"
-        info += first + " - " + last
+        info += first_edit + " - " + last_edit
         #
         return info
 
     def get_title(self):
+        """Return a copy of _title"""
         return self._title
 
-    def get_desc(self):
-        return self._desc
+    def get_description(self):
+        """Return a short description of the Book"""
+        return self._meta['desc']
 
     def get_authors(self):
+        """Return a copy of the _author list"""
         return self._author.copy()
 
     def get_meta(self):
+        """Return a copy of the _meta dict"""
         return self._meta.copy()
 
     def edit_title(self, title):
+        """Edit and (re)format the title of the Book"""
         self._title = str(title).strip()
         return None
 
     def edit_description(self, description):
-        self._desc = str(description).strip()
+        """Edit and (re)format a short description of the Book"""
+        self._meta['desc'] = str(description).strip()
         return None
 
     def add_author(self, author):
-        self._author.append(str(author).strip())
+        """Add a (re)formatted Author to the _author list"""
+        self._author.append(str(author).title().strip())
         return None
 
     def add_meta(self, tag, text):
+        """Add a (re)formatted tag/text pair to the _meta dict"""
         self._meta[str(tag).strip()] = str(text).strip()
         return None        
 
     def add_branch(self, node=None, text=None):
+        """Add new node to specified existing node.
+
+           Input Arguments:
+               node - ID of node to branch off.  Default is START_OF_BOOK
+               text - Text to associate with newly created node.  Default is ""
+
+           Returns:
+               ID of newly created node
+
+        """
         node = node or self._BEGIN
         assert(node in self._nodes)
         new_node = Node()
@@ -85,11 +111,30 @@ class Book(object):
         return new_id
 
     def terminate_branch(self, node):
+        """Add link to connect specified node to END_OF_BOOK
+
+           Input Arguments:
+               node - ID of node to connect to END_OF_BOOK
+
+           Returns:
+               None
+
+        """
         assert(node in self._nodes)
         self._nodes[node].add_link(self._END)
         return None
 
     def insert_branch(self, node, break_pt):
+        """Break a specified node in two, thus introducing a new branch point.
+
+           Input Arguments:
+               node     - ID of node to insert branch point
+               break_pt - location in node for new branch point
+
+           Returns:
+               ID of original node
+
+        """
         assert(node in self._nodes)
         assert(0 < break_pt < len(self._nodes[node].get_text()))
         new_node = Node()
@@ -102,7 +147,20 @@ class Book(object):
         return node
 
     def dump(self, mode):
-        text = ""
+        """Create a representation of the Book.
+
+           Input Arguments:
+               mode - method for determining how to traverse the nodes
+                      "RANDOM"  - create representation following random nodes [default]
+                      "OLD"     - create representation following oldest (original) nodes
+                      "NEW"     - create representation following newest nodes
+                      "POPULAR" - create representation following most popular nodes
+
+           Returns:
+               text string
+
+        """
+        text = str()
         node = self._BEGIN
         while node != self._END:
             text += self._nodes[node].get_text() + " "
@@ -120,5 +178,14 @@ class Book(object):
         return text.strip()
 
     def save(self, filename):
+        """Serialize (pickle) the Book and write it to file.
+
+           Input Arguments:
+               filename - relative path to output file
+
+           Returns:
+               absolute path to created pickle file
+
+        """
         pickle.dump(self, open(filename, "w"))
         return os.path.abspath(filename)
